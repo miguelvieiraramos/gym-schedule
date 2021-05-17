671b8f8e53aa3a461d550c32119fe414ef8d8d2a
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-05-16 22:17:58 -0300

    feat: move ApiErrorsMixin to utils

--- handlers.py
+++ handlers.py
@@ -1,12 +1,17 @@
 from rest_framework.views import exception_handler
 
+from utils import ErrorsFormatter
+
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
+    # If an unexpected error occurs (server error, etc.)
     if response is None:
         return response
 
-    response.data = {'detail': exc.detail[0]}
+    formatter = ErrorsFormatter(exc)
+
+    response.data = formatter()
 
     return response
