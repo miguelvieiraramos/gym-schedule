4015c0c79724fe63dbc6830f3fe9b43a854472dd
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-05-19 21:20:32 -0300

    fix: fix IsAuthenticatedOrCreateOnly permission

--- gym_schedule/permissions.py
+++ gym_schedule/permissions.py
@@ -4,4 +4,4 @@ from rest_framework.permissions import BasePermission
 class IsAuthenticatedOrCreateOnly(BasePermission):
 
     def has_permission(self, request, view):
-        return bool(request.method == 'POST')
+        return bool((request.user and request.user.is_authenticated) or request.method == 'POST')
